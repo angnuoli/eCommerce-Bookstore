f51f4663a55589a30ea7b96d793fcd909dce2d8d
Alan <angnuoli@gmail.com>
2017-11-25 21:56:41 -0500

    Add services for checkout controller.

---
+++ Bookstore/src/main/java/com/bookstore/service/PaymentServiceImpl.java
@@ -0,0 +1,20 @@
+package com.bookstore.service;
+
+import org.springframework.stereotype.Service;
+
+import com.bookstore.domain.Payment;
+import com.bookstore.domain.UserPayment;
+
+@Service
+public class PaymentServiceImpl implements PaymentService {
+	public Payment setByUserPayment(UserPayment userPayment, Payment payment) {
+		payment.setType(userPayment.getType());
+		payment.setHolderName(userPayment.getHolderName());
+		payment.setCardNumber(userPayment.getCardNumber());
+		payment.setExpiryMonth(userPayment.getExpiryMonth());
+		payment.setExpiryYear(userPayment.getExpiryYear());
+		payment.setCvc(userPayment.getCvc());
+		
+		return payment;
+	}
+}
