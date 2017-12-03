be37242f73dc1dba02494e8bcda227dcb1eff3b6
Alan <angnuoli@gmail.com>
2017-12-03 03:52:24 -0500

    Delete adminportal
    Merge adminportal into bookstore.

--- adminportal/src/main/java/com/adminportal/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.adminportal.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.adminportal.domain.Book;
-
-public interface BookRepository extends CrudRepository<Book, Long>{
-	
-}
