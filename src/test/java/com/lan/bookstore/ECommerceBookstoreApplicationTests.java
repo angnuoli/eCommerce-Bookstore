c4fbd74bb1a5019f3d71a8cc613be833167a1064
angnuo <angnuoli@gmail.com>
2018-03-19 01:37:27 -0400

    Add HomeController Test
    1. add faqTest()

--- src/test/java/com/lan/bookstore/ECommerceBookstoreApplicationTests.java
+++ src/test/java/com/lan/bookstore/ECommerceBookstoreApplicationTests.java
@@ -3,11 +3,9 @@ package com.lan.bookstore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"/config/application-context.xml"})
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ECommerceBookstoreApplicationTests {
 
--- src/test/java/com/lan/bookstore/controller/customerController/HomeControllerTest.java
+++ src/test/java/com/lan/bookstore/controller/customerController/HomeControllerTest.java
@@ -1,38 +1,56 @@
 package com.lan.bookstore.controller.customerController;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
 /**
  *
  */
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration(locations = {"/config/application-context.xml"})
-public class HomeControllerTest {
-  //  private final String USER_AGENT = "Mozilla/5.0";
 
-//    @Test
-//    public void testFaq() throws IOException {
-//        String url = "http://localhost:8080/faq";
-//        URL obj = new URL(url);
-//        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-//
-//        con.setRequestMethod("POST");
-//        con.setRequestProperty("User-Agent", USER_AGENT);
-//
-//        int responseCode = con.getResponseCode();
-//        System.out.println("\nSending 'POST' request to URL : " + url);
-//        System.out.println("Response Code: " + responseCode);
-//
-//        BufferedReader in = new BufferedReader(
-//                new InputStreamReader(con.getInputStream()));
-//        String inputLine;
-//        StringBuffer response = new StringBuffer();
-//
-//        while ((inputLine = in.readLine()) != null) {
-//            response.append(inputLine);
-//        }
-//        in.close();
-//
-//        //print result
-//        System.out.println(response.toString());
-//
-//    }
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(locations = {
+        "classpath*:/config/application-context.xml",
+})
+public class HomeControllerTest {
+    private MockMvc mvc;
+
+
+//    @Autowired
+//    HomeController homeController;
+
+    @Mock
+    private HomeController homeController;
+
+    @Before
+    public void setUp() throws Exception {
+
+        MockitoAnnotations.initMocks(this);
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB_INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        mvc = MockMvcBuilders.standaloneSetup(homeController).setViewResolvers(viewResolver).build();
+    }
+
+    @Test
+    public void faqTest() throws Exception {
+        mvc.perform(post("/faq").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(view().name("faq"));
+    }
 }
